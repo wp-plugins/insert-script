057052756d1b4e6473b62a655d7ebd4c6200cc53
aaronbelchamber <aaronbelchamber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-08 13:19:46 +0000

    Tested with WP 4.3, added $json_string array that gets passed to your script the $json_vals_arr array from your JSON string.
    git-svn-id: https://plugins.svn.wordpress.org/insert-script/trunk@1215794 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- insert-script.php
+++ insert-script.php
@@ -2,14 +2,15 @@
 
 /**
  * @package Insert_Script
- * @version 1.1
+ * @version 1.3
  */
 /*
 Plugin Name: Insert Script
 Plugin URI: http://belchamber.us/wordpress/plugins/insert-script.zip
-Description: With the [insert_script] shortcode, this calls on the absolute path of any script from the path you provide with the "path=" attribute.
+Description: With the [insert_script] shortcode, this calls on the absolute path of any script from the path you provide with the "path=" attribute.  Now allows "vals" and "strings" as GET URL attribute,
+             "json_string" for properly encoded JSON string arrays will return $json_vals_arr to your script, and passes on "save_db" and "section" as $save_db and $section variables to whatever values you assign them to your external scripts.
 Author: Aaron Belchamber
-Version: 1.1
+Version: 1.3
 Author URI: http://www.belchamber.us
 */
 
@@ -42,10 +43,42 @@ if (!class_exists("InsertScript")) {
         }
 
         function shortcode($atts){
-            extract(shortcode_atts(array("path" => '',"vars"=>''), $atts));
+            /*
+             * Dynamically include file from path
+             *
+             * $path  String
+             * example [insert_script path='/resources/main.php'] (paths start at public_html)
+             *
+             * $strings   Uses PHP parse_str function -- look it up
+             *
+             */
+
             ob_start();
-            include($_SERVER['DOCUMENT_ROOT'].$path);
-            return ob_get_clean();
+
+            extract(shortcode_atts(array('path' => '','vals'=>'','strings' => '','save_db' => '','section'=>'','json_string'=>''), $atts));
+            $path=html_entity_decode(trim($path));
+            $strings=html_entity_decode(trim($strings));
+            $vals=html_entity_decode(trim($vals));
+            $save_db=html_entity_decode(trim($save_db));
+            $section=html_entity_decode(trim($section));
+
+            $json_string=html_entity_decode(trim($json_string));
+            $json_vals_arr=json_decode($json_string,true);
+
+            if ($strings!=''){
+                parse_str($strings);// Assign strings before including file to parse to inject values
+            }
+
+            if ($vals!=''){
+                parse_str($vals);
+            }
+
+            if ($path!=''){
+                @include($_SERVER['DOCUMENT_ROOT'].$path);
+            }
+
+            $html=ob_get_clean();
+            return $html;
         }
 
     }
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: aaronbelchamber
 Donate link: http://belchamber.us/wordpress/plugins
 Tags: insert,shortcode,insert script,php
 Requires at least: 3.0.1
-Tested up to: 4.2.2
+Tested up to: 4.3
 Stable tag: /trunk/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -13,7 +13,11 @@ With the [insert_script] shortcode you can call on other local code.
 
 With the [insert_script] shortcode, this calls on the absolute path of any script from the path you provide with the "path=" attribute.
 It can also pass along declared variables from a "vars=" attribute where you can define GET values in URL safe declarations -- whatever
-the script you are calling on requires.  This allows a level of back-end integration with code, processes, and data outside of Wordpress.
+the script you are calling on requires.  
+
+Now allows "vals" and "strings" you define with GET URL attribute (var1=value1&var2=value2).
+Also pass JSON Array via "json_string" for properly encoded JSON string arrays which will pass array $json_vals_arr to your script, and passes on "save_db" and "section" as $save_db and $section variables to whatever values you assign them to your external scripts.
+This allows a level of back-end integration with code, processes, and data outside of Wordpress.
 
 
 == Installation ==
@@ -28,4 +32,6 @@ the script you are calling on requires.  This allows a level of back-end integra
 
 == Changelog ==
 
-No changes made in  past two years and still works with every version of Wordpress to date.
+1.3 Added new variables and ability to declare scalable "json_string" which will pass $json_vals_arr JSON array to your script too.
+
+1.1 No changes made in  past two years and still works with every version of Wordpress to date.
